229729202861bff314442801c56c469b8ab6403f
wzmyyj <2209011667@qq.com>
2018-08-22 20:50:05 +0800

    new 58

--- app/src/main/java/top/wzmyyj/zymk/app/utils/GlideImageLoader.java
+++ app/src/main/java/top/wzmyyj/zymk/app/utils/GlideImageLoader.java
@@ -3,9 +3,11 @@ package top.wzmyyj.zymk.app.utils;
 import android.content.Context;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.youth.banner.loader.ImageLoader;
 
-import top.wzmyyj.zymk.app.tools.G;
+import top.wzmyyj.zymk.R;
 
 
 /**
@@ -15,6 +17,10 @@ import top.wzmyyj.zymk.app.tools.G;
 public class GlideImageLoader extends ImageLoader {
     @Override
     public void displayImage(Context context, Object path, ImageView imageView) {
-        G.img(context, (String) path, imageView);
+        Glide.with(context)
+                .load((String)path)
+//                .placeholder(R.mipmap.ic_progress)
+                .apply(new RequestOptions().centerCrop().error(R.mipmap.ico_bg))
+                .into(imageView);
     }
 }
